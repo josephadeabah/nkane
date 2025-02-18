77d09fa99db2b42b6d6ac5dc6a2cd694cb49b639
josephadeabah <abeansah.lx@gmail.com>
2025-02-18 18:30:07 +0000

    feat: add more categories

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
     prevArrow: <PrevArrow onClick={() => {}} />, // Custom previous arrow
     nextArrow: <NextArrow onClick={() => {}} />, // Custom next arrow
     autoplay: true, // Enable autoplay
-    autoplaySpeed: 5000, // Set the time for each slide to change (3000ms = 3 seconds)
+    autoplaySpeed: 5000, // Set the time for each slide to change (5000ms = 5 seconds)
     responsive: [
       {
         breakpoint: 1024,
@@ -101,6 +101,194 @@ export default function Home() {
       image: '/hero-bg.jpg',
       description:
         'Discover stories from Central African countries like Cameroon, Congo, and Gabon.',
+    },
+    {
+      title: 'Horn of Africa',
+      image: '/hero-bg.jpg',
+      description:
+        'Explore the vibrant cultures of the Horn of Africa, including Somalia, Eritrea, and Djibouti.',
+    },
+    {
+      title: 'Sahel',
+      image: '/hero-bg.jpg',
+      description:
+        'Learn about the semi-arid region of the Sahel, spanning countries like Mali, Niger, and Chad.',
+    },
+    {
+      title: 'Indian Ocean Islands',
+      image: '/hero-bg.jpg',
+      description:
+        'Uncover the stories from the island nations of the Indian Ocean, including Madagascar, Mauritius, and Seychelles.',
+    },
+    {
+      title: 'Saharan Africa',
+      image: '/hero-bg.jpg',
+      description:
+        'Delve into the nomadic traditions and desert cultures of Saharan countries like Libya, Tunisia, and Mauritania.',
+    },
+  ];
+
+  const tribes = [
+    {
+      title: 'Yoruba',
+      image: '/hero-bg.jpg',
+      description:
+        'Explore the rich mythology and folklore of the Yoruba people from Nigeria.',
+    },
+    {
+      title: 'Zulu',
+      image: '/hero-bg.jpg',
+      description:
+        'Discover the vibrant traditions and stories of the Zulu nation in Southern Africa.',
+    },
+    {
+      title: 'Maasai',
+      image: '/hero-bg.jpg',
+      description:
+        'Learn about the Maasai warriors and their oral traditions from Kenya and Tanzania.',
+    },
+    {
+      title: 'Hausa',
+      image: '/hero-bg.jpg',
+      description:
+        'Listen to the tales of the Hausa people, one of the largest ethnic groups in West Africa.',
+    },
+    {
+      title: 'Xhosa',
+      image: '/hero-bg.jpg',
+      description:
+        'Experience the storytelling traditions of the Xhosa people from South Africa.',
+    },
+    {
+      title: 'Berber',
+      image: '/hero-bg.jpg',
+      description:
+        'Uncover the ancient stories of the Berber people from North Africa.',
+    },
+    {
+      title: 'Igbo',
+      image: '/hero-bg.jpg',
+      description:
+        'Explore the myths and legends of the Igbo people from Nigeria.',
+    },
+    {
+      title: 'San',
+      image: '/hero-bg.jpg',
+      description:
+        'Discover the ancient storytelling traditions of the San people of Southern Africa.',
+    },
+    {
+      title: 'Fulani',
+      image: '/hero-bg.jpg',
+      description:
+        'Learn about the nomadic Fulani people and their oral traditions across West Africa.',
+    },
+    {
+      title: 'Ashanti',
+      image: '/hero-bg.jpg',
+      description:
+        'Delve into the rich history and traditions of the Ashanti people from Ghana.',
+    },
+    {
+      title: 'Nzema',
+      image: '/hero-bg.jpg',
+      description:
+        'Delve into the rich history and traditions of the Nzema people from Ghana.',
+    },
+    {
+      title: 'Fanste',
+      image: '/hero-bg.jpg',
+      description:
+        'Delve into the rich history and traditions of the Ashanti people from Ghana.',
+    },
+    {
+      title: 'Dagbani',
+      image: '/hero-bg.jpg',
+      description:
+        'Delve into the rich history and traditions of the Dagbani people from Ghana.',
+    },
+    {
+      title: 'Tutsi',
+      image: '/hero-bg.jpg',
+      description:
+        'Explore the culture, history, and traditions of the Tutsi people from Rwanda and Burundi.',
+    },
+    {
+      title: 'Shona',
+      image: '/hero-bg.jpg',
+      description:
+        'Learn about the Shona people and their ancient history from Zimbabwe.',
+    },
+    {
+      title: 'Tuareg',
+      image: '/hero-bg.jpg',
+      description:
+        'Uncover the tales and traditions of the Tuareg people, nomads of the Sahara.',
+    },
+    {
+      title: 'Wolof',
+      image: '/hero-bg.jpg',
+      description:
+        'Discover the language, culture, and traditions of the Wolof people in Senegal and The Gambia.',
+    },
+    {
+      title: 'Ndebele',
+      image: '/hero-bg.jpg',
+      description:
+        'Explore the traditions and vibrant art of the Ndebele people from Zimbabwe and South Africa.',
+    },
+  ];  
+
+  const themes = [
+    {
+      title: 'Folklore',
+      image: '/hero-bg.jpg',
+      description:
+        'Explore traditional stories passed down through generations.',
+    },
+    {
+      title: 'Myths',
+      image: '/hero-bg.jpg',
+      description:
+        'Discover ancient myths that explain the origins of the world and natural phenomena.',
+    },
+    {
+      title: 'Legends',
+      image: '/hero-bg.jpg',
+      description:
+        'Listen to legendary tales of heroes, warriors, and mythical creatures.',
+    },
+    {
+      title: 'Moral Lessons',
+      image: '/hero-bg.jpg',
+      description:
+        'Learn valuable life lessons through stories with moral teachings.',
+    },
+    {
+      title: 'Adventure',
+      image: '/hero-bg.jpg',
+      description:
+        'Embark on thrilling adventures with stories of exploration and discovery.',
+    },
+    {
+      title: 'Romance',
+      image: '/hero-bg.jpg',
+      description: 'Experience tales of love, passion, and heartbreak.',
+    },
+    {
+      title: 'Historical Events',
+      image: '/hero-bg.jpg',
+      description: 'Relive significant historical events through storytelling.',
+    },
+    {
+      title: 'Modern Adaptations',
+      image: '/hero-bg.jpg',
+      description: 'Enjoy contemporary retellings of traditional stories.',
+    },
+    {
+      title: 'Spiritual',
+      image: '/hero-bg.jpg',
+      description: 'Explore stories with spiritual and religious themes.',
     },
   ];
 
@@ -232,7 +420,7 @@ export default function Home() {
       {/* Category by Regions Carousel */}
       <section className="py-20 overflow-hidden relative">
         <div className="w-full">
-          <h2 className="text-3xl font-bold text-center mb-12">
+          <h2 className="text-3xl font-bold text-left px-6 mb-4">
             Category by Regions
           </h2>
           <Slider {...carouselSettings}>
@@ -244,9 +432,61 @@ export default function Home() {
                     alt={region.title}
                     className="w-full h-96 object-cover rounded-lg"
                   />
-                  <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4 rounded-b-lg">
+                  <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-100 text-white p-4 rounded-b-lg">
                     <h3 className="text-xl font-bold">{region.title}</h3>
                     <p className="text-sm">{region.description}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </section>
+
+      {/* Category by Tribes Carousel */}
+      <section className="py-20 overflow-hidden relative">
+        <div className="w-full">
+          <h2 className="text-3xl font-bold text-left px-6 mb-4">
+            Category by Tribes
+          </h2>
+          <Slider {...carouselSettings}>
+            {tribes.map((tribe, index) => (
+              <div key={index} className="px-2">
+                <div className="relative">
+                  <img
+                    src={tribe.image}
+                    alt={tribe.title}
+                    className="w-full h-96 object-cover rounded-lg"
+                  />
+                  <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-100 text-white p-4 rounded-b-lg">
+                    <h3 className="text-xl font-bold">{tribe.title}</h3>
+                    <p className="text-sm">{tribe.description}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </section>
+
+      {/* Category by Themes Carousel */}
+      <section className="py-20 overflow-hidden relative">
+        <div className="w-full">
+          <h2 className="text-3xl font-bold text-left px-6 mb-4">
+            Category by Themes
+          </h2>
+          <Slider {...carouselSettings}>
+            {themes.map((theme, index) => (
+              <div key={index} className="px-2">
+                <div className="relative">
+                  <img
+                    src={theme.image}
+                    alt={theme.title}
+                    className="w-full h-96 object-cover rounded-lg"
+                  />
+                  <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-100 text-white p-4 rounded-b-lg">
+                    <h3 className="text-xl font-bold">{theme.title}</h3>
+                    <p className="text-sm">{theme.description}</p>
                   </div>
                 </div>
               </div>
