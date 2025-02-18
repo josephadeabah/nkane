af4b0587aeaf0b81d0b0846d0fc35592575bc07d
josephadeabah <abeansah.lx@gmail.com>
2025-02-18 21:46:09 +0000

    fix: image height

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bantuhive-space-media.nyc3.digitaloceanspaces.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
