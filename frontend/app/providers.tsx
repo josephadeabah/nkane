d807edd9ff8278828c1b3c8eaa506df88eacd88e
josephadeabah <abeansah.lx@gmail.com>
2025-03-08 21:49:42 +0000

    fix: type errors

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
